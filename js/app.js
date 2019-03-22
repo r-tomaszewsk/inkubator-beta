451c3e276ee24f76c2843e81942e9e061a71c1b3
adamo222 <adambodek@gmail.com>
2019-03-22 12:55:36 +0100

    added modelCode modules and function under Enter button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 const buttonInput = document.querySelector("#buttonInput");
-const buttonModel = document.querySelector("#buttonShow");
+const buttonShow = document.querySelector("#buttonShow");
 const buttonBack = document.querySelector("#buttonBack");
 
 let home = document.querySelector('#home');
 let second = document.querySelector('#second');
 
+let inputModelCode = document.querySelector('#inputModelCode');
+
 buttonInput.addEventListener("click", function(){
+    
+    let modelCode = inputModelCode.value;
+    console.log(modelCode);
+    let result = app.modelCode.parse(modelCode);
+    console.log(result);
     home.style.display = "none";
     second.style.display = "block";
 });
-buttonModel.addEventListener("click", function() {
+buttonShow.addEventListener("click", function() {
     console.log("my model");
 })
 buttonBack.addEventListener("click", function() {
