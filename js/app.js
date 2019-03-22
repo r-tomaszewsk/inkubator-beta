1c83c657055630f0bc201130213c55d21cfbc5c8
Damian <d.rzepiejews@samsung.com>
2019-03-22 11:39:51 +0100

    funcionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const button = document.querySelector("#buttonInput");
 const buttonModel = document.querySelector("#buttonShow");
 
 button.addEventListener("click", function(){
+    home.style.display = "none"
+    second.style.display = "block"
     console.log("Wpisz");
 });
 buttonModel.addEventListener("click", function() {
