74426df9afa555c75da7a93b6e0e55199a03e92a
adamo222 <adambodek@gmail.com>
2019-03-29 10:13:55 +0100

    fix index

--- builder.bat
+++ builder.bat
@@ -3,7 +3,7 @@ call cd export
 call tizen package -t wgt -s haslo123 -- ".buildResult"
 call mv .buildResult ..\.buildResult
 call cd ..
-call sdb connect 192.168.0.102
+call sdb connect 192.168.0.109
 call sdb push .buildResult\InkubatorBeta.wgt /home/owner/share/tmp/sdk_tools/tmp/
 call sdb shell 0 vd_appinstall appis /home/owner/share/tmp/sdk_tools/tmp/InkubatorBeta.wgt
 call sdb shell 0 debug FYEPxWQpTx.InkubatorBeta 300
